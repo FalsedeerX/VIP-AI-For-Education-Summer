1bac8e173afc991e5bb700767a7203265184ac18
Kaya Tacer <ktacer@proton.me>
2025-05-31 19:02:15 -0700

    Update index.py

--- instructorchat/retrieval/index.py
+++ instructorchat/retrieval/index.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from typing import Optional
 
 from haystack.core.pipeline import Pipeline
 from haystack.components.routers.file_type_router import FileTypeRouter
@@ -10,10 +11,10 @@ from haystack.components.writers.document_writer import DocumentWriter
 from haystack.components.joiners.document_joiner import DocumentJoiner
 from haystack_integrations.document_stores.chroma import ChromaDocumentStore
 
-def index(docs_dir: str = "documents") -> ChromaDocumentStore:
+def build_document_store(docs_dir: str = "documents", persistent_path: Optional[str] = None) -> ChromaDocumentStore:
     file_paths = [docs_dir / Path(name) for name in os.listdir(docs_dir)]
 
-    document_store = ChromaDocumentStore()
+    document_store = ChromaDocumentStore(persist_path=persistent_path)
 
     indexing = Pipeline()
     indexing.add_component("router", FileTypeRouter(["text/plain", "application/pdf", "text/markdown"]))
@@ -37,4 +38,4 @@ def index(docs_dir: str = "documents") -> ChromaDocumentStore:
     return document_store
 
 if __name__ == "__main__":
-    print(index().search(["How do you make a histogram?"], 3))
+    print(build_document_store().search(["How do you make a histogram?"], 3))
