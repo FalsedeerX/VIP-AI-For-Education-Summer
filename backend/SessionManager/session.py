6ec74d39e25ff5ad3fb7b8e1bf50cd92135dfa64
Falsedeer <chen5292@purdue.edu>
2025-06-05 21:36:25 -0400

    Create Outline for SessionWorker

--- backend/SessionManager/session.py
+++ backend/SessionManager/session.py
@@ -24,7 +24,10 @@ class SessionManager:
 
 	def assign_token(self, username: str, time_to_live: int) -> UUID:
 		""" Assigning authenciated user with an UUID as the session token. """
-		return uuid4()
+		token = uuid4()
+		key_name = f"session:{token}"
+
+		return token
 
 
 	def verify_token(self, username: str, ip_address: str, token: UUID) -> bool:
@@ -48,32 +51,49 @@ class SessionManager:
 
 
 class SessionWorker:
-	def __init__(self, config: ValkeyConfig):
+	def __init__(self, config: ValkeyConfig, scan_interval_seconds: int = 60, idle_timeout_seconds: int = 3600):
 		self.db = redis.Redis(host=config.db_host, port=config.db_port, db=config.db_index, 
 							  username=config.db_user, password=config.db_pass)
+		self.scan_interval = scan_interval_seconds
+		self.idle_timeout = idle_timeout_seconds
 		self.stop_event = threading.Event()
-		self.threads = []
+		self.config = config
+		self._threads = []
 
 		# database connection test
 		self.db.ping()
 
 
-	def start(self):
+	def start(self) -> None:
 		""" Start the IDLE cleanup worker & expire cleanup worker in the background. """
-		pass
+		idle_cleanup_thread = threading.Thread(target=self._idle_cleanup_worker,
+											   args=(self.scan_interval, self.idle_timeout),
+											   name="IdleCleanupWorker",
+											   daemon=True)
+
+		expire_cleanup_thread = threading.Thread(target=self._expire_cleanup_worker,
+												 name="ExpireCleanupWorker",
+												 daemon=True)
+
+		# log the threads
+		self._threads.extend([idle_cleanup_thread, expire_cleanup_thread])
 
 
-	def stop(self):
+	def stop(self) -> bool:
 		""" Stop all the created thread worker gracefully. """
-		pass
+		self.stop_event.set()
+		for worker in self._threads:
+			worker.join()
+
+		return True
 
 
-	def _idle_cleanup_worker(self):
+	def _idle_cleanup_worker(self, scan_interval_seconds: int, idle_timeout_seconds: int) -> None:
 		""" Scan through the database a few minutes to terminate unactive session token. """
 		pass
 
 
-	def _expire_cleanup_worker(self):
+	def _expire_cleanup_worker(self) -> None:
 		""" Event callback triggered when a key expired in database. """
 		pass
 
@@ -81,4 +101,5 @@ class SessionWorker:
 
 if __name__ == "__main__":
 	config = ValkeyConfig("localhost", 6379)
-	print(config)
+	manager = SessionManager(config)
+
