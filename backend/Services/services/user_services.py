b156c3194f92302fac6f9cb76fe166fabc7afb43
Falsedeer <chen5292@purdue.edu>
2025-06-17 08:59:16 -0400

    Update Function Naming in User Endpoints

--- backend/Services/services/user_services.py
+++ backend/Services/services/user_services.py
@@ -6,8 +6,8 @@ router = APIRouter(prefix="/users", tags=["users"])
 agent = DatabaseAgent()
 
 
-@router.post("/", status_code=201)
-async def api_create_user(payload: UserCreate):
+@router.post("/register", status_code=201)
+async def create_user(payload: UserCreate):
     """Create a new folder for a user."""
     try:
         user_id = await agent.register_user(payload.username, payload.email, payload.password)
@@ -17,21 +17,21 @@ async def api_create_user(payload: UserCreate):
 
 
 @router.get("/id/{username}", response_model=int)
-async def api_get_user_id(username: str):
+async def get_user_id(username: str):
     user_id = await agent.get_user_id(username)
     if user_id is None:
         raise HTTPException(404, "User not found")
     return user_id
 
 
-@router.get("/{user_id}", response_model=bool)
-async def api_verify_user(payload: UserLogin):
+@router.get("/auth/{user_id}", response_model=bool)
+async def verify_user(payload: UserLogin):
     is_verified = await agent.verify_user(payload.user_id, payload.password)
     return is_verified
 
 
 @router.delete("/delete/{user_id}", status_code=204)
-async def api_delete_user(payload: UserLogin):
+async def delete_user(payload: UserLogin):
     ok = await agent.delete_user(payload.username)
     if not ok:
         raise HTTPException(404, "User not found")
