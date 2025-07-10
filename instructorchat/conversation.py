6b2b76ee759769c0468a9db73d14b547328af62a
Kaya Tacer <ktacer@proton.me>
2025-07-10 13:47:49 -0700

    Fix image url

--- instructorchat/conversation.py
+++ instructorchat/conversation.py
@@ -39,7 +39,7 @@ class Message:
             else:
                 openai_content.append({
                     "type": "image_url",
-                    "image_url": {"url": content_part[1]}
+                    "image_url": {"url": f"data:image/jpeg;base64,{content_part[1]}"}
                 })
 
         return {
